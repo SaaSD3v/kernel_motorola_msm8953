824aa329734cab6cc943d7b1733cf1aa3a29e215
SaaSD3v <hearesaas@gmail.com>
2026-08-31 00:36:32 -0400

    fix CONFIG_ANDROID_PARANOID_NETWORK broken wifi
    (cherry picked from commit 9a7416218ae637c4120c417b31428bb0747fdfdf)

--- security/commoncap.c
+++ security/commoncap.c
@@ -31,9 +31,7 @@
 #include <linux/binfmts.h>
 #include <linux/personality.h>
 
-#ifdef CONFIG_ANDROID_PARANOID_NETWORK
 #include <linux/android_aid.h>
-#endif
 
 /*
  * If a non-root user executes a setuid-root binary in
@@ -82,12 +80,12 @@ int cap_capable(const struct cred *cred, struct user_namespace *targ_ns,
 {
 	struct user_namespace *ns = targ_ns;
 
-#ifdef CONFIG_ANDROID_PARANOID_NETWORK
+	/* Preserve Android legacy network-group capabilities even with
+	 * CONFIG_ANDROID_PARANOID_NETWORK disabled for container networking. */
 	if (cap == CAP_NET_RAW && in_egroup_p(AID_NET_RAW))
 		return 0;
 	if (cap == CAP_NET_ADMIN && in_egroup_p(AID_NET_ADMIN))
 		return 0;
-#endif
 
 	/* See if cred has the capability in the target user namespace
 	 * by examining the target user namespace and all of the target
